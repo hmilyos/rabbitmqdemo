e831ef3dd5fd059956d1cc55498075cd99bed2f7
oushiming <oushiming@sxt.com>
2018-10-31 23:43:07 +0800

    first commit

---
+++ rabbitmq-api/src/main/java/com/hmily/rabbitmqapi/RabbitmqApiApplication.java
@@ -0,0 +1,12 @@
+package com.hmily.rabbitmqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqApiApplication.class, args);
+	}
+}
