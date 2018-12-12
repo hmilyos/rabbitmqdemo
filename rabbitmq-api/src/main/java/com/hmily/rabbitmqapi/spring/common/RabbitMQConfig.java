7f351a3f7fdb37c0084cde99c418976e4fcf6b12
oushiming <oushiming@sxt.com>
2018-12-13 00:00:53 +0800

    rabbitAdmin init

--- rabbitmq-api/src/main/java/com/hmily/rabbitmqapi/spring/common/RabbitMQConfig.java
+++ rabbitmq-api/src/main/java/com/hmily/rabbitmqapi/spring/common/RabbitMQConfig.java
@@ -1,12 +1,14 @@
 package com.hmily.rabbitmqapi.spring.common;
 
+
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.hmily.rabbitmqapi.common.RabbitMQCommon;
-import com.rabbitmq.client.ConnectionFactory;
 
 
 @Configuration
@@ -22,4 +24,23 @@ public class RabbitMQConfig {
         connectionFactory.setVirtualHost(RabbitMQCommon.RABBITMQ_DEFAULT_VIRTUAL_HOST);
         return connectionFactory;
     }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return  rabbitAdmin;
+    }
+
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     * FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+     * HeadersExchange ：通过添加属性key-value匹配
+     * DirectExchange:按照routingkey分发到指定队列
+     * TopicExchange:多关键字匹配
+     */
+
+
 }
