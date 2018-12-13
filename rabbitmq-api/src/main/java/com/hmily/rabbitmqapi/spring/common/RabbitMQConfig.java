75feee7ac218f795b5dd3160159918d242619398
欧世明（深圳联友科技有限公司IS事业部项目一部制造领域) <ly-oushim@dfl.com.cn>
2018-12-13 19:38:01 +0800

    rabbitAdmin init

--- rabbitmq-api/src/main/java/com/hmily/rabbitmqapi/spring/common/RabbitMQConfig.java
+++ rabbitmq-api/src/main/java/com/hmily/rabbitmqapi/spring/common/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.hmily.rabbitmqapi.spring.common;
 
 
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
@@ -9,6 +10,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.hmily.rabbitmqapi.common.RabbitMQCommon;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
 
 
 @Configuration
@@ -33,7 +38,7 @@ public class RabbitMQConfig {
     }
 
     /**
-     * 针对消费者配置
+     *  针对消费者配置
      * 1. 设置交换机类型
      * 2. 将队列绑定到交换机
      * FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
@@ -42,5 +47,51 @@ public class RabbitMQConfig {
      * TopicExchange:多关键字匹配
      */
 
+    /**
+     *  声明 TopicExchange 类型的交换机 topic001
+     * @return
+     */
+    @Bean
+    public TopicExchange exchange001() {
+//    	是否持久化，是否自动删除
+		return new TopicExchange("topic001", true, false);
+	}
+    
+//    声明 queue001 队列
+    @Bean
+    public Queue queue001() {
+    	return new Queue("queue001", true); //队列持久
+    }
+    
+//    将上面的交换机和队列绑定
+    @Bean
+    public Binding binding001() {
+    	return BindingBuilder.bind(queue001()).to(exchange001()).with("spring.*");
+    }
+    
+    @Bean
+    public TopicExchange exchange002() {
+    	return new TopicExchange("topic002", true, false);
+    }
+    
+    @Bean
+    public Queue queue002() {
+    	return new Queue("queue002", true);
+    }
+    
+    @Bean
+    public Binding binding002() {
+    	return BindingBuilder.bind(queue002()).to(exchange002()).with("rabbit.*");
+    }
+    
+    @Bean
+    public Queue queue003() {
+        return new Queue("queue003", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding003() {
+        return BindingBuilder.bind(queue003()).to(exchange001()).with("mq.*");
+    }
 
 }
--- rabbitmq-api/src/test/java/com/hmily/rabbitmqapi/RabbitmqApiApplicationTests.java
+++ rabbitmq-api/src/test/java/com/hmily/rabbitmqapi/RabbitmqApiApplicationTests.java
@@ -2,6 +2,12 @@ package com.hmily.rabbitmqapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,4 +19,16 @@ public class RabbitmqApiApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Autowired
+	private RabbitAdmin rabbitAdmin;
+
+	@Test
+	public void testAdmin() throws Exception {
+		rabbitAdmin.declareExchange(new DirectExchange("test.direct", false, false));
+		rabbitAdmin.declareExchange(new TopicExchange("test.topic", false, false));
+		rabbitAdmin.declareExchange(new FanoutExchange("test.fanout", false, false));
+		rabbitAdmin.declareQueue(new Queue("test.direct.queue", false));
+		rabbitAdmin.declareQueue(new Queue("test.topic.queue", false));
+		rabbitAdmin.declareQueue(new Queue("test.fanout.queue", false));
+	}
 }
